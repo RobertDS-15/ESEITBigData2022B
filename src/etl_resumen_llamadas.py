c4730a3faf7ab40be083ae84afc7682afda62d1c
RobertDS-15 <robert.cortes015@gmail.com>
2022-10-19 01:49:27 +0000

    inclusion de logs

--- src/etl_resumen_llamadas.py
+++ src/etl_resumen_llamadas.py
@@ -9,11 +9,21 @@ from pkgutil import get_data
 import pandas as pd
 import os
 from pathlib import Path
+import logging
 
 #root_dir = Path(".").resolve()
 bucket = 'gs://rcortes_bucket_llamadas123'
 
 def main(): #funcion principal
+    # Basic configuration for logging
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s' #Hora de ejecucion, nombre de la funcion, nivel(adv, aviso etc)
+    logging.captureWarnings(True)
+    logging.basicConfig(
+        level=logging.INFO, 
+        format=log_fmt,
+        filename='data/logs/etl_llamadas.log')
+    
+    
     filename = 'llamadas123_julio_2022.csv'
     # leer archivo
     data = get_data(filename)
@@ -21,6 +31,8 @@ def main(): #funcion principal
     df_resumen = get_summary(data)
     # guarde el resumen
     save_data(df_resumen, filename)
+    
+    logging.info('DONE!!!')
 
 def save_data(df, filename):
     out_name = 'resumen_' + filename
@@ -51,11 +63,17 @@ def get_summary(data):
     return df_resume
 
 def get_data(filename):
+    logger = logging.getLogger('get_data') #Genero el logger de la funcion a ejecutar
+    
     data_dir = "raw"
     #root_dir=Path(".").resolve()#.parent
     filepath = os.path.join(bucket, "data", data_dir, filename)
+    
+    logger.info(f'Reading file: {filepath}') #La informacion 
 
     data = pd.read_csv(filepath, encoding='latin-1', sep=';') #leer el archivo en la ruta
+    
+    logger.info(f'La tabla contiene {data.shape[0]} filas y {data.shape[1]} columnas') # la tabla contiene tantas columna tantas filas 
     return data
 
 if __name__== '__main__':
